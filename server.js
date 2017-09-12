5be8f9bdc346e343d5ebb7a885a23f055c2860c9
Daniel Henning <daniel@puresick.me>
2017-09-12 22:35:42 +0200

    pw hasing testing

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require(`express`)
 const passport = require(`passport`)
-const sequelize = require('sequelize')
+const sequelize = require(`sequelize`)
+const crypto = require(`crypto`)
 
 const server = express()
 
 const config = require(`./config.json`)
 
-//DATANASE CONFIGURATION
+//DATABASE CONFIGURATION
 
 const db = new sequelize(config.db.database, config.db.user, config.db.pass, {
 	host: config.db.host,
@@ -26,10 +27,25 @@ db.authenticate()
 		console.log(err)
 	})
 
+const User = db.define(`user`, {
+	email: {
+		type: sequelize.STRING,
+		validate: {
+			isEmail: true
+		}
+	},
+	password: {
+		type: sequelize.TEXT
+	},
+	salt: {
+		type: sequelize.STRING
+	}
+})
+
 
 //PASSPORT CONFIGURATION
 
-const LocalStrategy = require(`passport-local`).Strategy
+//const LocalStrategy = require(`passport-local`).Strategy
 
 //passport.use(new LocalStrategy(
 //	function (username, password, done) {
@@ -41,6 +57,32 @@ const LocalStrategy = require(`passport-local`).Strategy
 
 server.get(`/`, (req, res) => {
 	res.send('hw')
+})
+
+server.get(`/create`, (req, res) => {
+	const username = `user@name.de`
+	const plainPassword = `deineMama123`
+	const salt = crypto.randomBytes(128).toString(`base64`)
+	const iterations = 100000
+	const hash = crypto.pbkdf2Sync(plainPassword, salt, iterations, 512, `sha512`)
+
+	User.sync({force: true})
+		.then(() => {
+			return User.create({
+				email: username,
+				password: hash,
+				salt: salt
+			})
+		})
+		.then(() => {
+			console.log(`Query findAll`)
+			User.findAll()
+				.then((users) => {
+					//console.log(users)
+				})
+		})
+
+	res.end()
 })
 
 server.post(`/login`, passport.authenticate(`local`), (req, res) => {
