8e4c2b130641a0e7e354ca6242266dae80e6f534
Daniel Henning <daniel@puresick.me>
2017-09-13 23:02:34 +0200

    [WIP] Login form authentication

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require(`express`)
 const passport = require(`passport`)
 const sequelize = require(`sequelize`)
 const crypto = require(`crypto`)
+const path = require(`path`)
+const bodyParser = require(`body-parser`)
+
+//NEEDS REVIEW
+const flash = require(`connect-flash`)
+const session = require(`express-session`)
 
 const server = express()
 
@@ -27,7 +33,7 @@ db.authenticate()
 		console.log(err)
 	})
 
-const User = db.define(`user`, {
+const User = db.define(`users`, {
 	email: {
 		type: sequelize.STRING,
 		validate: {
@@ -44,53 +50,119 @@ const User = db.define(`user`, {
 
 
 //PASSPORT CONFIGURATION
+server.use(passport.initialize())
+server.use(bodyParser.urlencoded())
+server.use(bodyParser.json())
 
-//const LocalStrategy = require(`passport-local`).Strategy
+const LocalStrategy = require(`passport-local`).Strategy
 
-//passport.use(new LocalStrategy(
-//	function (username, password, done) {
-//
-//	}
-//))
+//registration passport strategy
+
+
+passport.use(`login`, new LocalStrategy({
+		passReqToCallback: true,
+		usernameField: `email`
+	},
+	function (req, username, password, done) {
+		console.log(`logincheck`)
+		console.log(username)
+		User.findOne({ where: { email: username } })
+			.then((user) => {
+				console.log(`then user`)
+				console.log(user)
+				if (user == null) {
+					return done(null, false, { message: `Username or password incorrect` })
+				}
+
+				console.log(`password`)
+				console.log(password)
+				console.log(user.salt)
+				const hash = crypto.pbkdf2Sync(password, user.salt, 100000, 512, `sha512`).toString(`base64`)
+
+				console.log(`hash`)
+				console.log(hash)
+				console.log(`user password db`)
+				console.log(user.password)
+
+				if (user.password === hash) {
+					return done(null, user)
+				}
+
+				return done(null, false, { message: `login error` })
+			})
+	}
+))
 
 //SERVER ROUTING
 
-server.get(`/`, (req, res) => {
-	res.send('hw')
+server.get(`/suc`, (req, res) => {
+	res.send(`suc`)
 })
 
-server.get(`/create`, (req, res) => {
-	const username = `user@name.de`
-	const plainPassword = `deineMama123`
-	const salt = crypto.randomBytes(128).toString(`base64`)
-	const iterations = 100000
-	const hash = crypto.pbkdf2Sync(plainPassword, salt, iterations, 512, `sha512`)
+server.get(`/nonoo`, (req, res) => {
+	res.send(`nonoo`)
+})
 
+server.get(`/resetDB`, (req, res) => {
 	User.sync({force: true})
 		.then(() => {
-			return User.create({
-				email: username,
-				password: hash,
-				salt: salt
+			res.send(`db reset`)
+			res.end()
+		})
+})
+
+server.post(`/register`, (req, res) => {
+	let createUser = function () {
+		User.findOne({ where: { email: req.body.email } })
+			.then((user) => {
+				console.log(`user`)
+				console.log(user)
+
+				if (user) {
+					console.log(`User already registered`)
+				} else {
+					const salt = crypto.randomBytes(128).toString(`base64`)
+					const hash = crypto.pbkdf2Sync(req.body.password, salt, 100000, 512, `sha512`).toString(`base64`)
+
+					User.create({
+						email: req.body.email,
+						password: hash,
+						salt: salt
+					})
+					.then((user) => {
+						console.log(user)
+						console.log(`User ${user.email} created`)
+					})
+				}
 			})
-		})
-		.then(() => {
-			console.log(`Query findAll`)
-			User.findAll()
-				.then((users) => {
-					//console.log(users)
-				})
-		})
+			.catch((err) => {
+				console.error(`Database error:`, err)
+			})
+	}
 
-	res.end()
+	process.nextTick(createUser)
 })
 
-server.post(`/login`, passport.authenticate(`local`), (req, res) => {
-	console.log(`Auth successful`)
-	console.log(req.user)
+server.post(`/login`, passport.authenticate(`login`, {
+	successRedirect: `/suc`,
+	failureRedirect: `/nonoo`
+}))
+
+
+//FORM TEST
+//
+//
+//
+
+server.use(express.static(path.join(__dirname, `html`)))
+const htmlDir = `./html`
+
+server.get(`/`, (req, res) => {
+	res.sendFile(`/html/form.html`, { root: __dirname })
 })
 
-
+//
+//
 //SERVER CONFIGURATION
 
 server.listen(config.port, () => {
