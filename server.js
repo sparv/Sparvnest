7867bd27e438570c5b587520b243a5277e75b754
Daniel Henning <daniel@puresick.me>
2017-10-03 18:48:04 +0200

    removed session dependencies and modules

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const passport = require(`passport`)
 const crypto = require(`crypto`)
 const path = require(`path`)
 const bodyParser = require(`body-parser`)
-const cookieParser = require(`cookie-parser`)
 const flash = require(`connect-flash`)
-const session = require(`express-session`)
-const MemoryStore = require(`memorystore`)(session)
 
 //SPARVNEST COMPONENTS
 const initDb = require(`${__dirname}/components/database/init`)
 const tables = require(`${__dirname}/components/database/tables`)
 const initLoginStrategy = require(`${__dirname}/components/authentication/login`)
-const initUserSessions = require(`${__dirname}/components/authentication/usersession`)
 const initRouting = require(`${__dirname}/components/routing/routes`)
 
 const config = require(`./config.json`)
@@ -25,22 +21,13 @@ const db = initDb(config)
 const User = tables.User(db)
 
 initLoginStrategy(User)
-initUserSessions(User)
 
 //FORM TEST
 server.use(express.static(path.join(__dirname, `html`)))
 
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
-server.use(cookieParser()) //obsolet depending on express-session repo docs
-server.use(session({
-	store: new MemoryStore({
-		checkPeriod: 86400000
-	}),
-	secret: `supersecretneedstochange`
-}))
 server.use(passport.initialize())
-server.use(passport.session())
 
 initRouting(server, User, config)
 
