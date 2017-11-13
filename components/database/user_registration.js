baa1762e7254498a5c7b5dd4e44933409fb633a0
Daniel Henning <daniel@puresick.me>
2017-11-13 18:13:05 +0100

    moved user registration logic completely into own component

--- components/database/user_registration.js
+++ components/database/user_registration.js
@@ -1,11 +1,15 @@
 const hashPassword = require(`./hash_password`)
 const createSalt = require(`./create_salt`)
 
-function userRegistration (dbTable, data, callback) {
+function userRegistration (response, dbTable, data) {
   dbTable.findOne({ where: { email: data.email } })
   .then((user) => {
     if (user) {
-      callback(null, false, null)
+      return response
+        .status(403)
+        .send({
+          message: `User not registered - there is already a user registered with this email adress`
+        })
     } else {
       const salt = createSalt()
       dbTable.create({
@@ -15,12 +19,21 @@ function userRegistration (dbTable, data, callback) {
         forename: data.forename,
         surname: data.surname
       }).then((user) => {
-        callback(null, true, user.email)
+        return response
+          .status(200)
+          .send({
+            email: data.email
+          })
       })
     }
   })
   .catch((err) => {
-    console.error(`Database error:`, err)
+    console.log(err)
+    return response
+      .status(500)
+      .send({
+        message: `An error happened on the server side`
+      })
   })
 }
 
