c826d57277f8337385082e1cb4493a4f5020fff6
Daniel Henning <daniel@puresick.me>
2017-11-14 20:32:01 +0100

    rewrote user_update function to include authentication and changes api specs

--- components/database/user_update.js
+++ components/database/user_update.js
@@ -4,42 +4,108 @@ const hashPassword = require(`./hash_password`)
 const jwt = require(`jsonwebtoken`)
 
 function userUpdate (request, response, tableUsers, config) {
-  const data = request.body
-
-  tableUsers.findOne({ where: { email: data.current.email } })
-  .then((user) => {
-    let updateData = {}
-    if (data.update.email !== null) updateData[`email`] = data.update.email
-    if (data.update.password !== null) updateData[`password`] = hashPassword(data.update.password, user.salt)
-
-    tableUsers.update(updateData, {
-      where: { email: user.email }
-    })
-    .then(() => {
-      tableUsers.findOne({ where: { email: data.update.email } })
-      .then((updatedUser) => {
-        const token = jwt.sign({
-          sub: `user_autentication`,
-          name: updatedUser.email,
-          relation_id: updatedUser.relation_id
-        }, config.auth.secret)
-
+  if ((token !== `undefined`) && (token !== ``)) {
+    jwt.verify(token, config.auth.secret, (err, verification) => {
+      if (err) {
+        console.log(err)
         return response
-          .status(200)
+          .status(401)
           .send({
-            message: `User data was updated`,
-            token: token
+            message: `User authentication failed`
           })
-      })
+      }
+
+      if (request.body.meta === undefined && request.body.security === undefined) {
+        return response
+          .status(400)
+          .send({
+            message: `The data sent is invalid - no meta or security object found`
+          })
+      } else if (request.body.meta !== undefined && request.body.security !== undefined) {
+        return response
+          .status(400)
+          .send({
+            message: `The data sent is invalid - only one object is possible, meta or security`
+          })
+      } else if (request.body.meta !== undefined && request.body.security === undefined) {
+        const data = request.body.meta
+
+        let updateData = {}
+
+        if (data.forename !== null) updateData[`forename`] = data.forename
+        if (data.surname !== null) updateData[`surname`] = data.surname
+        if (data.email !== null) updateData[`email`] = data.email
+
+        tableUsers.update(updateData, { where: { email: verification.email } })
+          .then(user => {
+            console.log(`updated user promise response`)
+
+            const token = jwt.sign({
+              sub: `user_autentication`,
+              name: user.email,
+              relation_id: user.relation_id
+            }, config.auth.secret)
+
+            return response
+              .status(200)
+              .send({
+                message: `User data was updated`,
+                token: token
+              })
+          })
+          .catch(() => {
+            return response
+              .status(500)
+              .send({
+                message: `Database Error - User was not updated`
+              })
+          })
+      } else if (request.body.meta === undefined && request.body.security !== undefined) {
+        const data = request.body.security
+
+        tableUsers.findOne({ where: { email: validation.email } })
+          .then(user => {
+            const password = {
+              old: hashPassword(data.password_old, user.salt),
+              new: hashPassword(data.password_new, user.salt)
+            }
+
+            if (user.password === password.old) {
+              tableUsers.update({ password: password.new }, {
+                where: { email: user.email }
+              })
+                .then(() => {
+                  const token = jwt.sign({
+                    sub: `user_autentication`,
+                    name: user.email,
+                    relation_id: user.relation_id
+                  }, config.auth.secret)
+
+                  return response
+                    .status(200)
+                    .send({
+                      message: `User password was updated`,
+                      token: token
+                    })
+                })
+            } else {
+              return response
+                .status(401)
+                .send({
+                  message: `Authentication failed - current password is associated with this user`
+                })
+            }
+          })
+          .catch(() => {
+            return response
+              .status(500)
+              .send({
+                message: `Database Error: User not found`
+              })
+          })
+      }
     })
-    .catch(() => {
-      return response
-        .status(500)
-        .send({
-          message: `Internal Server error`
-        })
-    })
-  })
+  }
 }
 
 module.exports = userUpdate
