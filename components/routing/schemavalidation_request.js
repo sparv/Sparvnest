57e48ea5d058e1ba25dc80cf8373efb2befe4161
Daniel Henning <daniel@puresick.me>
2017-11-23 22:57:05 +0100

    [FeierAbend] added first Joi validation logic to components - user_update currently broken with this commit

--- components/routing/schemavalidation_request.js
+++ components/routing/schemavalidation_request.js
@@ -1,99 +1,99 @@
-const joi = require('joi')
+const Joi = require('joi')
 
 const schema = {
-  user_login: Joi.object().keys({}),
-  user_registration: Joi.object().keys({
+  // user_login: Joi.object().keys({}),
+  user_registration: {
+    requestBody: Joi.object().keys({
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+        forename: Joi.string().regex(/^[a-zA-Z]+$/).required(),
+        surname: Joi.string().regex(/^[a-zA-Z]+$/).required()
+      })
+  },
+  user_profile_get: {
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    })
+  },
+  user_update: {
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    }),
     requestBody: {
-      email: Joi.string().email().required()
-      password: Joi.string().required()
-      forename: Joi.string().regex(/^[a-zA-Z]$/).required()
-      surname: Joi.string().regex(/^[a-zA-Z]$/).required()
-    }
-  }),
-  user_profile_get: Joi.object().keys({
-    requestHeader: {
-      token: Joi.string().required()
-    }
-  }),
-  user_update: Joi.object().keys({
-    requestHeader: {
-      token: Joi.string().required()
-    },
-    requestBody: {
-      meta: {
-        email: Joi.string().email()
-        password: Joi.string()
-        forename: Joi.string().regex(/^[a-zA-Z]$/)
-        surname: Joi.string().regex(/^[a-zA-Z]$/)
-      },
-      security: {
-        password_old: Joi.string()
+      meta: Joi.object().keys({
+        email: Joi.string().email(),
+        password: Joi.string(),
+        forename: Joi.string().regex(/^[a-zA-Z]+$/),
+        surname: Joi.string().regex(/^[a-zA-Z]+$/)
+      }),
+      security: Joi.object().keys({
+        password_old: Joi.string(),
         password_new: Joi.string()
-      }
+      })
     }
-  }),
-  user_delete: Joi.object().keys({
-    requestHeader: {
-      token: Joi.string().required()
-    },
-    requestBody: {
+  },
+  user_delete: {
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    }),
+    requestBody: Joi.object().keys({
       password: Joi.string().required()
-    }
-  }),
-  customer_all_get: Joi.object().keys({
-    requestHeader: {
-      token: Joi.string().required()
-    }
-  }),
-  customer_get: Joi.object().keys({
-    requestParams: {
+    })
+  },
+  customer_all_get: {
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    })
+  },
+  customer_get: {
+    requestParams: Joi.object().keys({
       customer_id: Joi.number().required()
-    },
-    requestHeader: {
-      token: Joi.string().required()
-    }
-  }),
-  customer_add: Joi.object().keys({
-    requestHeader: {
-      token: Joi.string().required()
-    },
-    requestBody: {
-      relation_id: Joi.number()
-      forename: Joi.string().regex(/^[a-zA-Z]$/)
-      surname: Joi.string().regex(/^[a-zA-Z]$/)
-      email: Joi.string().email()
-      phone: Joi.number()
-      gender: Joi.string()
+    }),
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    })
+  },
+  customer_add: {
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    }),
+    requestBody: Joi.object().keys({
+      relation_id: Joi.number(),
+      forename: Joi.string().regex(/^[a-zA-Z]+$/),
+      surname: Joi.string().regex(/^[a-zA-Z]+$/),
+      email: Joi.string().email(),
+      phone: Joi.number(),
+      gender: Joi.string(),
       age: Joi.string()
-    }
-  }),
-  customer_update: Joi.object().keys({
-    requestParams: {
+    })
+  },
+  customer_update: {
+    requestParams: Joi.object().keys({
       customer_id: Joi.number().required()
-    },
-    requestHeader: {
-      token: Joi.string().required()
-    },
-    requestBody: {
-      forename: Joi.string().regex(/^[a-zA-Z]$/)
-      surname: Joi.string().regex(/^[a-zA-Z]$/)
-      phone: Joi.number()
-      email: Joi.string().email()
-      gender: Joi.string()
-      age: Joi.string()
-    }
-  }),
-  customer_delete: Joi.object().keys({
-    requestParams: {
+    }),
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    }),
+    requestBody: Joi.object().keys({
+      forename: Joi.string().regex(/^[a-zA-Z]+$/),
+      surname: Joi.string().regex(/^[a-zA-Z]+$/),
+      phone: Joi.number(),
+      email: Joi.string().email(),
+      gender: Joi.string(),
+      age: Joi.string(),
+    })
+  },
+  customer_delete: {
+    requestParams: Joi.object().keys({
       customer_id: Joi.number().required()
-    },
-    requestHeader: {
-      token: Joi.string().required()
-    },
-    requestBody: {
-      surname: Joi.string().regex(/^[a-zA-Z]$/)
-    }
-  })
+    }),
+    requestHeader: Joi.object().keys({
+      token: Joi.string().regex(/^Bearer [a-zA-Z0-9.-]+$/).required()
+    }),
+    requestBody: Joi.object().keys({
+      surname: Joi.string().regex(/^[a-zA-Z]+$/)
+    })
+  }
 }
 
 module.exports = schema
