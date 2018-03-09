2e0122625a66cec3816c14474485007c8dd9689e
Daniel Henning <daniel@sparv.de>
2018-03-09 14:00:01 +0100

    removed empty line

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require(`express`)
 const bodyParser = require(`body-parser`)
 
 const routes = require(`./routes`)
-
 const config = require(`./server/config.json`)
 
 const server = express()
