1a580c8d3b6ec13cd72a53c7a46dfccba245fe89
Daniel Henning <daniel@sparv.de>
2018-03-21 19:59:00 +0100

    deleting all workoutexercises when deleting workoutplan

--- services/workoutplan/deletePlan.js
+++ services/workoutplan/deletePlan.js
@@ -3,6 +3,8 @@ const Joi = require(`joi`)
 const validateAccessToken = require(`../../lib/authentication/validateAccessToken`)
 const errorMap = require(`../../lib/helper/errorMap`)
 const workoutplanDelete = require(`../../lib/workoutplan/workoutplanDelete`)
+const workoutexerciseGetAll = require(`../../lib/workoutexercise/workoutexerciseGetAll`)
+const workoutexerciseDelete = require(`../../lib/workoutexercise/workoutexerciseDelete`)
 
 const schemaParams = Joi.string().regex(/^[a-zA-Z0-9-]+$/).required()
 
@@ -12,12 +14,19 @@ const deletePlan = async (request, response) => {
     const validationToken = await validateAccessToken(request.headers.authorization)
     const validateRequestParams = await Joi.validate(workoutplanId, schemaParams)
 
+    const gatheringExercises = await workoutexerciseGetAll(workoutplanId, validationToken.user_id)
+
+    for (const item of gatheringExercises) {
+      await workoutexerciseDelete(item.workoutexercise_id, validationToken.user_id)
+    }
+
     const deletion = await workoutplanDelete(workoutplanId, validationToken.user_id)
 
     return response
       .status(200)
       .send({ message: `Workoutplan deleted` })
   } catch (error) {
+    console.log(error)
     const mapping = errorMap(error)
 
     return response
