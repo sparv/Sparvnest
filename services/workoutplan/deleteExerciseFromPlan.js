7a290e9a5419e9eab8c09df3cf0de14660700cd7
Daniel Henning <daniel@sparv.de>
2018-03-21 19:09:46 +0100

    added DELETE route for workoutexercises

--- services/workoutplan/deleteExerciseFromPlan.js
+++ services/workoutplan/deleteExerciseFromPlan.js
@@ -2,6 +2,7 @@ const Joi = require(`joi`)
 
 const validateAccessToken = require(`../../lib/authentication/validateAccessToken`)
 const errorMap = require(`../../lib/helper/errorMap`)
+const workoutexerciseDelete = require(`../../lib/workoutexercise/workoutexerciseDelete`)
 
 const schemaParams = Joi.string().regex(/^[a-zA-Z0-9-]+$/).required()
 
@@ -9,9 +10,15 @@ const deleteExerciseFromPlan = async (request, response) => {
   try {
     const workoutexerciseId = request.params.workoutexerciseId
     const validationToken = await validateAccessToken(request.headers.authorization)
-    const validateParams = await Joi.validate(workoutexerciseIdId, schemaParams)
-  
+    const validateParams = await Joi.validate(workoutexerciseId, schemaParams)
+
+    const deletion = await workoutexerciseDelete(workoutexerciseId, validationToken.user_id)
+
+    return response
+      .status(200)
+      .send({ message: `Workoutexercise deleted` })
   } catch (error) {
+    console.log(error)
     const mapping = errorMap(error)
 
     return response
@@ -20,4 +27,4 @@ const deleteExerciseFromPlan = async (request, response) => {
   }
 }
 
-module.exporst = deleteExerciseFromPlan
+module.exports = deleteExerciseFromPlan
