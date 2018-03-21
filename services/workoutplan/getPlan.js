0bfb57324f9a268d3c0e39bc53ca9cbc5145f876
Daniel Henning <daniel@sparv.de>
2018-03-21 15:59:18 +0100

    added exercise meta to GET single workoutplan

--- services/workoutplan/getPlan.js
+++ services/workoutplan/getPlan.js
@@ -4,6 +4,7 @@ const validateAccessToken = require(`../../lib/authentication/validateAccessToke
 const errorMap = require(`../../lib/helper/errorMap`)
 const workoutplanGet = require(`../../lib/workoutplan/workoutplanGet`)
 const workoutexerciseGetAll = require(`../../lib/workoutexercise/workoutexerciseGetAll`)
+const exerciseGet = require(`../../lib/exercise/exerciseGet`)
 
 const schemaParams = Joi.string().regex(/^[a-zA-Z0-9-]+$/).required()
 
@@ -16,6 +17,20 @@ const getPlan = async (request, response) => {
     const plan = await workoutplanGet(workoutplanId, validationToken.user_id)
     const planExercises = await workoutexerciseGetAll(workoutplanId, validationToken.user_id)
 
+    //Needs await Promise.all or it wont be waited on the Promises inside the map function
+    const workoutexercisesList = await Promise.all(planExercises.map(async item => {
+      const getExerciseMeta = await exerciseGet(item.exercise_id)
+
+      return {
+        exercise_id: item.exercise_id,
+        name: getExerciseMeta.exercise.name,
+        level: getExerciseMeta.exercise.level,
+        description: getExerciseMeta.exercise.description,
+        weight: item.weight,
+        repetition: item.repetition
+      }
+    }))
+
     return response
       .status(200)
       .send({ workoutplan: {
@@ -23,7 +38,7 @@ const getPlan = async (request, response) => {
         customer_id: plan.customer_id,
         title: plan.title,
         description: plan.description,
-        workoutexercises_list: planExercises 
+        workoutexercises_list: workoutexercisesList
       }})
   } catch (error) {
     const mapping = errorMap(error)
