5dbe8e9221695dee0108d563e652dcb39c5fd865
Daniel Henning <daniel@sparv.de>
2018-03-28 12:51:49 +0200

    added exercise count to workoutplans and exercisegroup get routes

--- services/workoutplan/getAllPlans.js
+++ services/workoutplan/getAllPlans.js
@@ -4,33 +4,28 @@ const errorMap = require(`../../lib/helper/errorMap`)
 const workoutplanGetAll = require(`../../lib/workoutplan/workoutplanGetAll`)
 const workoutexerciseGetAll = require(`../../lib/workoutexercise/workoutexerciseGetAll`)
 
-const getAllPlans = (request, response) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const validationToken = await validateAccessToken(request.headers.authorization)
-      const gathering = await workoutplanGetAll(validationToken.user_id)
+const getAllPlans = async (request, response) => {
+  try {
+    const validationToken = await validateAccessToken(request.headers.authorization)
+    const gathering = await workoutplanGetAll(validationToken.user_id)
 
-      const planList = await Promise.all(gathering.map(async item => {
-        const workoutexerciseList = await workoutexerciseGetAll(item.workoutplan_id, validationToken.user_id)
-        item[`workoutexercise_count`] = workoutexerciseList.length
-        console.log(item)
+    const planList = await Promise.all(gathering.map(async item => {
+      const workoutexerciseList = await workoutexerciseGetAll(item.workoutplan_id, validationToken.user_id)
+      item[`workoutexercise_count`] = workoutexerciseList.length
 
-        return item
-      }))
+      return item
+    }))
 
-      resolve(response
-        .status(200)
-        .send({ workoutplans: planList })
-      )
-    } catch (error) {
-      const mapping = errorMap(error)
+    return response
+      .status(200)
+      .send({ workoutplans: planList })
+  } catch (error) {
+    const mapping = errorMap(error)
 
-      reject(response
-        .status(mapping.status)
-        .send(mapping)
-      )
-    }
-  })
+    return response
+      .status(mapping.status)
+      .send(mapping)
+  }
 }
 
 module.exports = getAllPlans
